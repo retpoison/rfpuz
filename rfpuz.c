1475ca58893316798db776ea2a11375abcc3c40b
retpoison <standardret@proton.me>
2024-09-22 18:51:29 +0330

    -s flag added for changing board size.

--- rfpuz.c
+++ rfpuz.c
@@ -1,66 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "external/fpuz.h"
 #include "raylib.h"
 
-#define BLK_SIZE 90
-#define PZL_SIZE 4
-#define FONTSIZE 30
-#define SPACING  4
-#define SCREENW  ((BLK_SIZE * PZL_SIZE) + SPACING)
-#define SCREENH  ((BLK_SIZE * PZL_SIZE) + 50)
+#define BLK_SIZE     90
+#define SPACING      4
+#define DEFSIZE      4
+#define NUMFONT      30
+#define TXTFONT      (SCREENW / 12)
+#define SCREENW      ((BLK_SIZE * game.pzl_size) + SPACING)
+#define SCREENH      ((BLK_SIZE * game.pzl_size) + TXTFONT)
+#define TOIND(A, B)  (A * game.pzl_size + B)
 
 struct Game {
-	int moves, win;
-	int board[PZL_SIZE][PZL_SIZE];
-	int lboard[PZL_SIZE * PZL_SIZE];
-	Color Background, Foreground;
-};
+	int moves, win, pzl_size;
+	int *board;
+	Color background, foreground;
+} game;
 
-struct Game
-init_game(void)
+void
+init_game(int size, int light)
 {
-	struct Game g = { 0, 0, {{0}}, {0}, BLACK, WHITE };
-	fp_set_size(PZL_SIZE);
-	fp_generate(g.board);
-	return g;
+	game = (struct Game) {
+		.moves = 0,.win = 0,.pzl_size = size,.board = NULL,
+		.background = BLACK,.foreground = WHITE
+	};
+	if (light) {
+		game.background = WHITE;
+		game.foreground = BLACK;
+	}
+
+	game.board = (int *)malloc(game.pzl_size * game.pzl_size * sizeof(int));
+	fp_set_size(game.pzl_size);
+	fp_generate((int (*)[])game.board);
+}
+
+void
+cleanup(void)
+{
+	free(game.board);
 }
 
 int
-check_win(int a[PZL_SIZE * PZL_SIZE])
+check_win(int *a)
 {
 	int i;
-	for (i = 1; i < PZL_SIZE * PZL_SIZE; i++)
+	for (i = 1; i < game.pzl_size * game.pzl_size; i++)
 		if (a[i - 1] >= a[i])
 			return 0;
 	return 1;
 }
 
 struct fp_Pos
-get_empty_pos(int a[PZL_SIZE][PZL_SIZE])
+get_empty_pos(int *a)
 {
 	int i, j;
-	for (i = 0; i < PZL_SIZE; i++)
-		for (j = 0; j < PZL_SIZE; j++)
-			if (a[i][j] == PZL_SIZE * PZL_SIZE)
+	for (i = 0; i < game.pzl_size; i++)
+		for (j = 0; j < game.pzl_size; j++)
+			if (a[TOIND(i, j)] == game.pzl_size * game.pzl_size)
 				return (struct fp_Pos) { i, j };
 	return (struct fp_Pos) { -1, -1 };
 }
 
 int
-move_cell(int a[PZL_SIZE][PZL_SIZE])
+move_cell(int *a)
 {
 	int i, j, abi, abj, x, y;
 	struct fp_Pos epos = { -1, -1 };
 	Vector2 mp = GetMousePosition();
 	x = (int)mp.x % BLK_SIZE;
 	y = (int)mp.y % BLK_SIZE;
-	if (x > BLK_SIZE - SPACING || x < SPACING ||
-	    y > BLK_SIZE - SPACING || y < SPACING)
+	if (x > BLK_SIZE - SPACING || x < SPACING || y > BLK_SIZE - SPACING
+	    || y < SPACING)
 		return 0;
 
 	j = mp.x / BLK_SIZE;
 	i = mp.y / BLK_SIZE;
-	if (i > PZL_SIZE || j >> PZL_SIZE)
+	if (i > game.pzl_size || j >> game.pzl_size)
 		return 0;
 
 	epos = get_empty_pos(a);
@@ -68,86 +85,103 @@ move_cell(int a[PZL_SIZE][PZL_SIZE])
 	abj = fp_abs(epos.j - j);
 	if ((abi == 0 && abj == 1)
 	    || (abi == 1 && abj == 0)) {
-		a[epos.i][epos.j] = a[i][j];
-		a[i][j] = PZL_SIZE * PZL_SIZE;
+		a[TOIND(epos.i, epos.j)] = a[TOIND(i, j)];
+		a[TOIND(i, j)] = game.pzl_size * game.pzl_size;
 		return 1;
 	}
 	return 0;
 }
 
 void
-draw(struct Game *g)
+draw(void)
 {
 	int i, j, blkx, blky, w;
 	BeginDrawing();
-	ClearBackground(g->Background);
-	if (!(g->win))
+	ClearBackground(game.background);
+	if (!(game.win))
 		goto draw_board;
-	w = MeasureText("You solved the puzzle", FONTSIZE) / 2;
+	w = MeasureText("You solved the puzzle", TXTFONT) / 2;
 	DrawText(TextFormat
 		 ("You solved the puzzle\n\nin %d move%c.\n\nClick to play again.",
-		  g->moves, (g->moves > 1) ? 's' : ' '), SCREENW / 2 - w,
-		 SCREENH / 2 - (FONTSIZE * 1.5), FONTSIZE, g->Foreground);
+		  game.moves, (game.moves > 1) ? 's' : ' '), SCREENW / 2 - w,
+		 SCREENH / 2 - (TXTFONT * 1.5), TXTFONT, game.foreground);
 	goto end;
 
  draw_board:
-	for (i = 0; i < PZL_SIZE; i++) {
-		for (j = 0; j < PZL_SIZE; j++) {
-			g->lboard[i * PZL_SIZE + j] = g->board[i][j];
-			if (g->board[i][j] == PZL_SIZE * PZL_SIZE)
+	for (i = 0; i < game.pzl_size; i++) {
+		for (j = 0; j < game.pzl_size; j++) {
+			if (game.board[TOIND(i, j)] ==
+			    game.pzl_size * game.pzl_size)
 				continue;
 			blky = (BLK_SIZE * i) + SPACING;
 			blkx = BLK_SIZE * j + SPACING;
-			DrawRectangleLines(blkx, blky,
-					   BLK_SIZE - SPACING,
-					   BLK_SIZE - SPACING, g->Foreground);
-			w = MeasureText(TextFormat("%d", g->board[i][j]),
-					FONTSIZE) / 2;
-			DrawText(TextFormat("%d", g->board[i][j]),
+			DrawRectangleLines(blkx, blky, BLK_SIZE - SPACING,
+					   BLK_SIZE - SPACING, game.foreground);
+			w = MeasureText(TextFormat
+					("%d", game.board[TOIND(i, j)]),
+					NUMFONT) / 2;
+			DrawText(TextFormat("%d", game.board[TOIND(i, j)]),
 				 blkx + ((BLK_SIZE - SPACING) / 2) - w,
 				 blky + ((BLK_SIZE - SPACING) / 2) -
-				 FONTSIZE / 2, FONTSIZE, g->Foreground);
+				 NUMFONT / 2, NUMFONT, game.foreground);
 		}
 	}
-	DrawText(TextFormat("Moves: %d\t\tEsc to exit.", g->moves),
-		 2, BLK_SIZE * PZL_SIZE, FONTSIZE, g->Foreground);
+	DrawText(TextFormat("Moves: %d\t\tEsc to exit.", game.moves), 2,
+		 BLK_SIZE * game.pzl_size, TXTFONT, game.foreground);
  end:
 	EndDrawing();
 }
 
-void
-update_game(struct Game *g)
+int
+update_game(void)
 {
 	if (IsMouseButtonPressed(0)) {
-		if (g->win) {
-			*g = init_game();
-		} else if (move_cell(g->board)) {
-			(g->moves)++;
+		if (game.win) {
+			return 1;
+		} else if (move_cell(game.board)) {
+			(game.moves)++;
 		}
 	}
-	g->win = check_win(g->lboard);
+	game.win = check_win(game.board);
+	return 0;
+}
+
+void
+usage(char *c)
+{
+	printf("usage: %s\n" "-l         light background\n"
+	       "-s number  size of board\n", c);
 }
 
 int
 main(int argc, char *argv[])
 {
-	struct Game game = init_game();
-	if (argc > 1) {
-		if (strcmp(argv[1], "-l") == 0
-		    || strcmp(argv[1], "--light") == 0) {
-			game.Background = WHITE;
-			game.Foreground = BLACK;
+	int i, sf = DEFSIZE, lf = 0;
+	for (i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "-l") == 0) {
+			lf = 1;
+		} else if (strcmp(argv[i], "-s") == 0) {
+			if (argv[i + 1]) {
+				sf = atoi(argv[i + 1]);
+				sf = (sf == 0) ? DEFSIZE : sf;
+				i++;
+			}
+		} else {
+			usage(argv[0]);
+			return 1;
 		}
 	}
-
+	init_game(sf, lf);
 	SetTraceLogLevel(LOG_WARNING);
 	InitWindow(SCREENW, SCREENH, "rfpuz");
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose()) {
-		update_game(&game);
-		draw(&game);
+		if (update_game())
+			init_game(sf, lf);
+		draw();
 	}
+	cleanup();
 	CloseWindow();
 	return 0;
 }
